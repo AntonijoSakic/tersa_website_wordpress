18cfe4992b49a5ef7bdef0b6a7bdce89b65d648e
Antonijo Sakic <antonijosakic@Antonijos-MBP.lan>
2024-04-20 19:11:53 +0200

    changes sent to blaz

---
+++ wp-content/themes/tersa_theme/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer" id="footer">
+        <div class="container">
+            <a href="#" class="brand">Tersa d.o.o.</a>
+            <ul class="menu">
+                <li>
+                    <a href="#">Naslovna</a> 
+                </li>
+                <li>
+                    <a href="#">O nama</a> 
+                </li>
+                <li>
+                    <a href="#">Montažne kuće</a> 
+                </li>
+                <li>
+                    <a href="#">Kontakt</a> 
+                </li>
+                <ul class="media-icons">
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-facebook"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                </ul>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique excepturi non repellat quae quas deserunt suscipit dolorem inventore nulla aperiam, aspernatur eveniet ut culpa obcaecati error iste vel? Fugiat, consequatur.
+                </p>
+            </ul>
+        </div>
+
+    </footer>
+    
+</body>
+</html>
---
+++ wp-content/themes/tersa_theme/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tersa d.o.o.</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <div id="footer">
+            <nav class="navbar">
+                <img src="images/tersalogo.png" class="logo">
+                </ul class="menu-links">
+                <li><a href="#">Naslovna</a></li>
+                <li><a href="#">O nama</a></li>
+                <li><a href="#">Proizvodi</a></li>
+                <li><a href="#">Montažne kuće</a></li>
+                <li><a href="#">Kontakt</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
